2b2ba0ce01ff2a0a64c94c0905b73cf50c54cebd
Tairesis <marcomarzos9@gmail.com>
2023-08-29 20:42:27 -0300

    Initial commit

---
+++ src/lab1/aula2/empresas/Empleado.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1.aula2.empresas;
+
+/**
+ *
+ * @author Marco
+ */
+public class Empleado {
+    String nombre;
+    String apellido;
+    int dni;
+    String categoria;
+    double sueldo;
+    Empresa empresa;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+    
+    
+    
+ 
+    
+}
---
+++ src/lab1/aula2/empresas/Empresa.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1.aula2.empresas;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Marco
+ */
+public class Empresa {
+
+    String razonSocial;
+    int cuit;
+    ArrayList<Empleado> empleados;
+    //----------------------------------------------------------------
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public int getCuit() {
+        return cuit;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public Empresa() {
+    }
+    //----------------------------------------------------------------
+    public void agregarEmpleado(Empleado e){
+        
+    }
+    public void mostrarEmpleados (){
+        
+    }
+
+}
